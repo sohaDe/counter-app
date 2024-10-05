0f75c474ad635997c69b275528030afcc1d60e5a
sohaDe <soha.debah@gmail.com>
2024-10-04 23:44:17 -0700

    Initial commit

---
+++ src/app/states/counter/counter.selector.ts
@@ -0,0 +1,7 @@
+import { createSelector } from "@ngrx/store";
+import { AppState } from "../app.state";
+
+export const selectCounterState = (state: AppState) => state.counter
+export const selectCount = createSelector(
+  selectCounterState,
+(state) => state.count)
